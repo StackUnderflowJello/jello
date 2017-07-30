4d21baf4afb666692c7382118a341a637a2b3cf6
JonathanLayssard <jonathanlayssard@gmail.com>
2017-07-30 12:41:07 -0400

    Adding updated POJO forget to put in constructors for two pojos

---
+++ src/main/java/com/revature/logging/Formatter.java
@@ -0,0 +1,11 @@
+package com.revature.logging;
+
+
+/**
+ * After receiving the message, the framework calls the Formatter with the message. The Formatter formats it for output
+ * @author Jonathan Layssard
+ *
+ */
+public class Formatter {
+
+}
