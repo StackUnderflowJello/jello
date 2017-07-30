3bbc71f9494af068ecabeed6524d3d6a5dc4694f
JonathanLayssard <jonathanlayssard@gmail.com>
2017-07-30 12:41:07 -0400

    Adding updated POJO forget to put in constructors for two pojos

---
+++ src/main/java/com/revature/logging/Handler.java
@@ -0,0 +1,17 @@
+package com.revature.logging;
+
+
+/**The framework then hands the formatted message to the appropriate Appender for disposition. 
+ * This might include a console display, writing to disk, appending to a database, or email.
+ *
+ * @author Jonathan Layssard
+ *
+ */
+
+public class Handler {
+
+	
+	
+	
+	
+}
---
+++ src/main/java/com/revature/logging/Logger.java
@@ -0,0 +1,15 @@
+package com.revature.logging;
+
+
+
+/**
+ * The Logger is responsible for capturing the message to be logged along with certain metadata and passing it to the logging framework
+ * @author Jonathan Layssard
+ *
+ */
+public class Logger {
+
+	
+	Logger logger;
+	
+}
