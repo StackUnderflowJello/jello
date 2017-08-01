6da48a2f7fbd374943e60f878525f81e476d8d9b
Michael Garza <michaelgarza496@gmail.com>
2017-08-01 10:58:26 -0400

    more impls

--- src/main/java/com/revature/dao/User_BoardDao.java
+++ src/main/java/com/revature/dao/User_BoardDao.java
@@ -1,18 +1,19 @@
 package com.revature.dao;
 
-import com.revature.pojo.User_Board;
+import com.revature.pojo.Board;
 
 public interface User_BoardDao {
 
 	//CREATE
-	
+	public void createBoard(Board board);
 	
 	//READ
-	
+	public String getBoardName(Board board);
 	
 	//UPDATE
+	public void changeBoardName(Board board);
 	
 	
 	//DELETE
-	
+	public void deleteBoard(Board board);
 }
