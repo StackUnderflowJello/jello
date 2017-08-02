ee7d3b92d78cb93c5961a669f3c10a3c7be465d0
Tom Scheffer <tom.scheffer@yahoo.com>
2017-08-02 14:41:42 -0400

    have burndown chart working with hardcoded values from server

--- src/main/java/com/revature/controller/BurndownCtrl.java
+++ src/main/java/com/revature/controller/BurndownCtrl.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.dto.HistoryDTO;
 
 @RestController
@@ -16,22 +18,23 @@ public class BurndownCtrl {
 
 	@RequestMapping(value="/burndown", method=RequestMethod.GET)
 	public ModelAndView openChart(){
-		System.out.println("finding MAV");
-		return new ModelAndView("/static/burndown.html");
+		return new ModelAndView("/static/features/burndown/burndown.html");
 	}
 	
 	@RequestMapping(value="/chart", method=RequestMethod.GET)
-	public @ResponseBody List<HistoryDTO> burnDown(){
+	public @ResponseBody String burnDown() throws JsonProcessingException{
 		List<HistoryDTO> json = new ArrayList<>();
 		json.add(new HistoryDTO("2017-07-24", 80));
 		json.add(new HistoryDTO("2017-07-25", 75));
 		json.add(new HistoryDTO("2017-07-26", 70));
-		//json.add(new HistoryDTO("2017-07-27", 70));
+		json.add(new HistoryDTO("2017-07-27", 70));
 		json.add(new HistoryDTO("2017-07-28", 70));
 		json.add(new HistoryDTO("2017-07-29", 68));
 		json.add(new HistoryDTO("2017-07-30", 60));
 		json.add(new HistoryDTO("2017-07-31", 60));
-		json.add(new HistoryDTO("2017-08-01", 62));
-		return json;
+		json.add(new HistoryDTO("2017-08-01", 58));
+		ObjectMapper mapper = new ObjectMapper();
+		String returnedJson = mapper.writeValueAsString(json);
+		return returnedJson;
 	}
 }
--- src/main/webapp/static/resources/js/controller.js
+++ src/main/webapp/static/resources/js/controller.js
@@ -1,24 +1,29 @@
 
 app.controller('ChartController', function($scope, chartService){
-   
-   $scope.getChart = chartService.getChart(function(response){$scope.chartItems = response.data})
-   var chart = json.parse(getChart);
+
+   $scope.getChart = chartService.getChart(function(response){var chart = response.data;
+   var chart = angular.fromJson(chart);
    var currPoints = [];
    var futurePoints = [];
    var firstDate = new Date(chart[0].date);
+   var minDate = new Date(firstDate.getFullYear(), firstDate.getMonth(), firstDate.getDate()+1);
+   
    var currDate = new Date();
    var maxPoints = chart[0].points;
    var endPoints = chart[chart.length - 1].points;
    
-   var numDaysWorked = Math.floor((currDate.getTime()-firstDate.getTime())/(1000*60*60*24));
+   var numDaysWorked = Math.floor((currDate.getTime()-minDate.getTime())/(1000*60*60*24));
    var avgPoints = ((maxPoints-endPoints)/numDaysWorked);
    for(i in chart ){
-      currPoints.push({x: new Date(chart[i].date), y:chart[i].points});
+	  var tempDate = new Date(chart[i].date);
+      currPoints.push({x: new Date(tempDate.getFullYear(), tempDate.getMonth(), tempDate.getDate()+1), y:chart[i].points});
    }
-   var finalDate = new Date();
-   finalDate.setDate(firstDate.getDate()+14);
+   var finalDate = new Date(minDate);
+   finalDate.setDate(finalDate.getDate()+14);
    var averagePoints = ((maxPoints-endPoints)/(currPoints.length-1));
-   for(var tempDate = new Date(currDate), tempPoints = endPoints; tempDate <= finalDate; tempDate.setDate(tempDate.getDate()+1), tempPoints -= averagePoints){
+   var tempFinalDate = new Date(finalDate);
+   tempFinalDate.setDate(tempFinalDate.getDate()+1);
+   for(var tempDate = new Date(currDate), tempPoints = endPoints; tempDate < tempFinalDate; tempDate.setDate(tempDate.getDate()+1), tempPoints -= averagePoints){
       if(tempPoints < 0){
          tempPoints = 0;
       }
@@ -39,7 +44,7 @@ app.controller('ChartController', function($scope, chartService){
       axisX:{
          labelFontSize: 16,
          gridThickness: 0,
-         minimum: firstDate,
+         minimum: minDate,
          title: "Dates"
       },
       data:[
@@ -74,5 +79,5 @@ app.controller('ChartController', function($scope, chartService){
    $scope.currDate = (currDate.getMonth()+1)+"/"+currDate.getDate()+"/"+currDate.getFullYear();
    $scope.avgPoints = (endPoints/(Math.floor((finalDate.getTime()-currDate.getTime())/(1000*60*60*24)))).toFixed(2);
    $scope.finalDate = (finalDate.getMonth()+1)+"/"+finalDate.getDate()+"/"+finalDate.getFullYear();
-   
+   });
 });
