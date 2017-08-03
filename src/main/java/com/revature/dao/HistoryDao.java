6798c0acdb2c6e41a48ab90d4c0d050dee89867f
Kosiba12 <kosibaoshodiglover@yahoo.com>
2017-08-02 21:45:41 -0400

    working project3

--- src/main/java/com/revature/dao/HistoryDao.java
+++ src/main/java/com/revature/dao/HistoryDao.java
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-package com.revature.dao;
-
-import com.revature.pojo.History;
-
-public interface HistoryDao {
-
-	//CREATE
-	public void saveHistory(History history);
-	
-	//READ
-	public History getHistory(History history);
-	
-	//UPDATE
-	public void updateHistory(History history);
-	
-	//DELETE
-	public void removeHistory(History history);
-}
-=======
 package com.revature.dao;
 
 import com.revature.pojo.History;
@@ -36,4 +16,3 @@ public interface HistoryDao {
     //DELETE
     public void removeHistory(History history);
 }
->>>>>>> b1ce11ba9a9950e10dde4b23d05e5a7a7a4de387
--- src/main/java/com/revature/dao/HistoryDaoImpl.java
+++ src/main/java/com/revature/dao/HistoryDaoImpl.java
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-package com.revature.dao;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.revature.pojo.History;
-
-public class HistoryDaoImpl implements HistoryDao {
-
-	@Autowired
-	private SessionFactory sessionFactory;
-	
-	@Override
-	public void saveHistory(History history) {
-		Session session = sessionFactory.getCurrentSession();
-		session.save(history);
-	}
-
-	@Override
-	public History getHistory(History history) {
-		Session session = sessionFactory.getCurrentSession();
-		return (History) session.get(History.class, history.getHistory_id());
-	}
-
-	@Override
-	public void updateHistory(History history) {
-
-		Session session = sessionFactory.getCurrentSession();
-		session.update(history);
-	}
-
-	@Override
-	public void removeHistory(History history) {
-		Session session = sessionFactory.getCurrentSession();
-		session.delete(history);
-	}
-
-}
-=======
 package com.revature.dao;
 
 
@@ -83,4 +42,3 @@ public class HistoryDaoImpl implements HistoryDao {
     }
 
 }
->>>>>>> b1ce11ba9a9950e10dde4b23d05e5a7a7a4de387
