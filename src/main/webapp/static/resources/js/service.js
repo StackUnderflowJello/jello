18417ea3c2381257d9bdca68de398c291fcb5a79
Nam Mai <nmai@mail.usf.edu>
2017-08-05 18:43:29 -0400

    Add Gil changes

--- src/main/webapp/static/resources/js/service.js
+++ src/main/webapp/static/resources/js/service.js
@@ -1,7 +1,7 @@
 app.service('chartService', function($http){
 	
 	this.getChart = function(callback){
-		$http.get('chart')
+		$http.get('static/chart')
 		.then(callback)
 		.catch(function(error){
 			
