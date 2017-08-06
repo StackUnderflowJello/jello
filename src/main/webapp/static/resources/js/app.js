01bc9e58ada882af224e26df83faa8610201b52d
Nam Mai <nmai@mail.usf.edu>
2017-08-06 11:59:30 -0400

    Add hide buttons/Add Gil's code

--- src/main/webapp/static/resources/js/app.js
+++ src/main/webapp/static/resources/js/app.js
@@ -34,10 +34,26 @@ app.config(function($routeProvider){
 		
 		.otherwise({
 			redirectTo: "/"
-		});
-		
-				
-	
+		});			
 	
 });
+
+
+app.controller("btnCtrl", function($scope){
+
+	$scope.showBtn = true;
+	
+	$scope.show = function(){
+		$scope.showBtn = true;
+		console.log("Showing btns");
+	}
+	
+	$scope.hide = function(){
+		$scope.showBtn = false;
+		console.log("Hiding btns");
+	}
+	
+})
+
+
 
